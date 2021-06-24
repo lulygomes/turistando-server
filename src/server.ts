976d5e94967ebd2e176d559f12374bc31705d5db
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-06-24 18:18:19 -0300

    Create data base

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import express, { response } from 'express';
+import createConnection from './database';
+
+createConnection();
+
+const app = express();
+
+app.use(express.json())
+
+app.use('/', (request, response) => (
+  response.json({ok: true})
+))
+
+const port = 3333;
+
+app.listen(port, () => console.log(`server on port ${port}`))
