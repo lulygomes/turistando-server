a93fc618e380f18248c49bace283272b2488571e
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-06-25 11:24:29 -0300

    create user service

---
+++ src/services/createUserServices.ts
@@ -0,0 +1,27 @@
+import { getRepository } from "typeorm"
+import User from "../models/Users"
+
+class CreateUserService {
+  public async execute(name: string): Promise<User | any> {
+    try {
+      const userRepository = getRepository(User);
+
+      const existUser = await userRepository.findOne({ where: { name }});
+
+      if (existUser) {
+        return existUser;
+      }
+
+      const user = userRepository.create({name});
+      await userRepository.save(user)
+
+
+      return User;
+    } catch (err) {
+      console.log(err)
+      return err
+    }
+  }
+}
+
+export default CreateUserService;
